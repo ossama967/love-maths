bace9b58101a9081b29430b2d694b0cf6988f15c
ossama Aldawamenah <ossamaomar@gmail.com>
2024-02-01 12:17:33 +0000

    Adg Favicon, Change Title, Get Enterkey to function as submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,12 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+
     runGame("addition");
 
 });
@@ -24,6 +30,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
@@ -122,7 +131,6 @@ function displaySubtractQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "-";
 
 }
-
 
 function displayMultiplyQuestion(operand1, operand2) {
 
